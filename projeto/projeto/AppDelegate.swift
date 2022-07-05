0b3c5b29b9fb384e7c335e9d04f9b8ec9f88ac40
geovannaks <geovanna.kasemirinski@gmail.com>
2022-07-05 09:49:58 -0300

    Retirada do Storyboard

--- projeto/projeto/AppDelegate.swift
+++ projeto/projeto/AppDelegate.swift
@@ -10,10 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let tabBarController = ViewController()
+        self.window?.rootViewController = tabBarController
+        window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
